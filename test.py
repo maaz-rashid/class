13ce5432f1b70123a2cbe1fcd9cddfbede73005e
maaz-rashid <maaz.rashid@outlook.com>
2018-07-29 11:34:33 +0500

    Initital commit

---
+++ test.py
@@ -0,0 +1,11 @@
+#%%
+print("There are 100 students in class.")
+print("There is a student's meeting today.")
+
+mango = 10
+apple = 50
+print("We have " + str(mango+apple) + " fruits in basket")
+a = 100
+print("there are "+str(a)+ " stdnts")
+
+print(f'We have {mango+apple} fruits in basket')
