8faede9ef480675938277367f2290ee1855d6884
maaz-rashid <maaz.rashid@outlook.com>
2018-08-15 02:25:09 +0500

    6 ques done

--- practice.py
+++ practice.py
@@ -64,3 +64,86 @@ num = int(input("Enter number for squares: "))
 for n in range(1,num):
     squares[n] = n*n
 print(squares)
+
+#----------------------------------------#
+# Question 4
+# Level 1
+
+# Question:
+# Write a program which accepts a sequence of comma-separated numbers from console and generate a list and a tuple which contains every number.
+# Suppose the following input is supplied to the program:
+# 34,67,55,33,12,98
+# Then, the output should be:
+# ['34', '67', '55', '33', '12', '98']
+# ('34', '67', '55', '33', '12', '98')
+
+# Hints:
+# In case of input data being supplied to the question, it should be assumed to be a console input.
+# tuple() method can convert list to tuple
+
+# Solution:
+
+string = input("Enter comma separated numbers: ")
+lst = string.split(",")
+tupl = tuple(lst)
+print(lst)
+print(tupl)
+
+#----------------------------------------#
+# Question 5
+# Level 1
+
+# Question:
+# Define a class which has at least two methods:
+# getString: to get a string from console input
+# printString: to print the string in upper case.
+# Also please include simple test function to test the class methods.
+
+# Hints:
+# Use __init__ method to construct some parameters
+
+# Solution:
+
+class processString(object):
+    def __init__(self):
+        self.s = ""
+
+    def getString(self):
+        self.s = input("")
+    def printString(self):
+        print(self.s)
+
+newObj = processString()
+newObj.getString()
+newObj.printString()
+
+#----------------------------------------#
+# Question 6
+# Level 2
+
+# Question:
+# Write a program that calculates and prints the value according to the given formula:
+# Q = Square root of [(2 * C * D)/H]
+# Following are the fixed values of C and H:
+# C is 50. H is 30.
+# D is the variable whose values should be input to your program in a comma-separated sequence.
+# Example
+# Let us assume the following comma separated input sequence is given to the program:
+# 100,150,180
+# The output of the program should be:
+# 18,22,24
+
+# Hints:
+# If the output received is in decimal form, it should be rounded off to its nearest value (for example, if the output received is 26.0, it should be printed as 26)
+# In case of input data being supplied to the question, it should be assumed to be a console input. 
+
+# Solution:
+import math
+
+inlist = input("Enter comma separated string of numbers: ")
+c = 50
+h = 30
+ans = []
+for d in inlist.split(","):
+    ans.append(str(int(math.sqrt((2*c*int(d)/h)))))
+print( ",".join(ans))
