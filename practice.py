67405d5bed2da51bbeaa22bfc08164561ddacdb5
maaz-rashid <maaz.rashid@outlook.com>
2018-08-16 19:48:40 +0500

    14 ques done

--- practice.py
+++ practice.py
@@ -241,3 +241,122 @@ string = input("Enter a sentence: ")
 word_list = string.split()
 print(" ".join(sorted(set(word_list))))
 
+#----------------------------------------#
+# Question 11
+# Level 2
+
+# Question:
+# Write a program which accepts a sequence of comma separated 4 digit binary numbers as its input and then check whether they are divisible by 5 or not. The numbers that are divisible by 5 are to be printed in a comma separated sequence.
+# Example:
+# 0100,0011,1010,1001
+# Then the output should be:
+# 1010
+# Notes: Assume the data is input by console.
+
+# Hints:
+# In case of input data being supplied to the question, it should be assumed to be a console input.
+
+# Solution:
+
+items = [x for x in input("Enter comma separated binary digits: ").split(",")]
+ans = []
+for x in items:
+    if (int(x,2))%5 == 0:
+        ans.append(x)
+
+print(",".join(ans))
+
+#----------------------------------------#
+# Question 12
+# Level 2
+
+# Question:
+# Write a program, which will find all such numbers between 1000 and 3000 (both included) such that each digit of the number is an even number.
+# The numbers obtained should be printed in a comma-separated sequence on a single line.
+
+# Hints:
+# In case of input data being supplied to the question, it should be assumed to be a console input.
+
+# Solution:
+ans = []
+for x in range(1000,3001):
+    strngx = str(x)
+    if (int(strngx[0])%2 == 0) and (int(strngx[1])%2 == 0) and (int(strngx[2])%2 == 0) and (int(strngx[3])%2 == 0):
+        ans.append(strngx)
+
+print(",".join(ans))
+
+#----------------------------------------#
+# Question 13
+# Level 2
+
+# Question:
+# Write a program that accepts a sentence and calculate the number of letters and digits.
+# Suppose the following input is supplied to the program:
+# hello world! 123
+# Then, the output should be:
+# LETTERS 10
+# DIGITS 3
+
+# Hints:
+# In case of input data being supplied to the question, it should be assumed to be a console input.
+
+# Solution:
+strInput = input("Enter a sentense: ")
+letterCount = 0
+numCount = 0
+for char in strInput:
+    if char.isdigit():
+        numCount += 1
+    elif char.isalpha():
+        letterCount +=1
+    else:
+        pass
+
+print(f"No. Letters: {letterCount}")
+print(f"No. Digits: {numCount}")
+
+#----------------------------------------#
+# Question 14
+# Level 2
+
+# Question:
+# Write a program that accepts a sentence and calculate the number of upper case letters and lower case letters.
+# Suppose the following input is supplied to the program:
+# Hello world!
+# Then, the output should be:
+# UPPER CASE 1
+# LOWER CASE 9
+
+# Hints:
+# In case of input data being supplied to the question, it should be assumed to be a console input.
+
+strInput = input("Enter a sentense: ")
+upperCount = 0
+lowerCount = 0
+for char in strInput:
+    if char.isupper():
+        upperCount += 1
+    elif char.islower():
+        lowerCount +=1
+    else:
+        pass
+
+print(f"UPPER CASE: {upperCount}")
+print(f"LOWER CASE: {lowerCount}")
+
+#----------------------------------------#
+# Question 15
+# Level 2
+
+# Question:
+# Write a program that computes the value of a+aa+aaa+aaaa with a given digit as the value of a.
+# Suppose the following input is supplied to the program:
+# 9
+# Then, the output should be:
+# 11106
+
+# Hints:
+# In case of input data being supplied to the question, it should be assumed to be a console input.
+
+# Solution:
